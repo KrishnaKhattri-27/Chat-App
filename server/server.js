156ab4b7e7f21d4263b74026f5bccdb381e9c218
Krishna <krishnakhattri24@gmail.com>
2024-04-24 17:59:41 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -15,8 +15,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-// app.use(cors({credentials: true, origin: "https://chat-app-lyart-rho.vercel.app",exposedHeaders: ["Set-Cookie"]}));
-app.use(cors());
+app.use(cors({credentials: true, origin: "https://chat-app-lyart-rho.vercel.app",exposedHeaders: ["Set-Cookie"]}));
+// app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
